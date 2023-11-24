36997dbc31b7803b82d9c86b19c88a94f01bdac2
Damián <panes.damianalberto@insucochillan.cl>
2023-11-24 12:23:19 -0300

    Se añade una carpeta para los componentes del UI y además se añade el componente de Navbar (de momento sin iconos) también se instala Tailwind CSS.

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import styles from './page.module.css'
+import Navbar from './ui/Navbar'
+import 'tailwindcss/tailwind.css'
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  )
+}
---
+++ src/app/ui/Navbar.jsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+const Navbar = () => {
+    return (
+        <nav className="bg-gray-800 p-4">
+        <button className=''>Menú</button>
+        <button className=''>Registrarme</button>
+        </nav>
+    )
+}
+
+export default Navbar;
