4a0f31b2a1a5510c83462e360d82fb4e2a91cce9
Damián <panes.damianalberto@insucochillan.cl>
2023-11-24 20:24:45 -0300

    Añadiendo las imagenes al header de la home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,17 @@
 import styles from './page.module.css'
+import './globals.css'
 import Navbar from './ui/Navbar'
 import 'tailwindcss/tailwind.css'
+
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <div className="header">
+        <img src="/images/home/static-waves.png" alt="" className="logo" />
+        <img src="/images/home/logo.png" alt="" className="waves" />
+      </div>
     </div>
   )
 }
