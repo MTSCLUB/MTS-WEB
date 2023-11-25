cd41a3883538dbc71f0cdc958934eac5c29801ab
Damián <panes.damianalberto@insucochillan.cl>
2023-11-24 21:18:04 -0300

    Añadiendo el css y corrigiendo los tamaños de icos

--- src/app/ui/Navbar.jsx
+++ src/app/ui/Navbar.jsx
@@ -1,10 +1,10 @@
 import Link from 'next/link'
-import styles from '../navbar.module.css'
+import styles from '../css/navbar.module.css'
 import { HiMiniBars3 } from "react-icons/hi2";
 const Navbar = () => {
     return (
         <nav className={styles.navbar}>
-            <button className={styles.button}><HiMiniBars3 /></button>
+            <button className={styles.button}><HiMiniBars3 size={25}/></button>
             <button className={styles.button}>Registrarme</button>
         </nav>
     )
