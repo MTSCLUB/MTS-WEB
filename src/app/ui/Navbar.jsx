d242a1e9015323441447a242ad31d4f669422aac
Damián <panes.damianalberto@insucochillan.cl>
2023-11-24 22:58:28 -0300

    Añadiendo el dropdown y funcionalidades del mismo

--- src/app/ui/Navbar.jsx
+++ src/app/ui/Navbar.jsx
@@ -1,12 +1,32 @@
+'use client'
+
 import Link from 'next/link'
 import styles from '../css/navbar.module.css'
 import { HiMiniBars3 } from "react-icons/hi2";
+import { HiOutlineXMark } from "react-icons/hi2";
+import { useState } from 'react';
+
 const Navbar = () => {
+    const [mostrarDropdown, setMostrarDropdown] = useState(false);
+
+    const toggleDropdown = () => {
+        setMostrarDropdown(!mostrarDropdown)
+    }
+
     return (
-        <nav className={styles.navbar}>
-            <button className={styles.button}><HiMiniBars3 size={25}/></button>
-            <button className={styles.button}>Registrarme</button>
-        </nav>
+        <div>
+            <nav className={styles.navbar}>
+                <button className={styles.button} onClick={toggleDropdown}>{mostrarDropdown ? <HiOutlineXMark size={25}/> : < HiMiniBars3 size={25}/>}</button>
+                <button className={styles.button}>Registrarme</button>
+            </nav>
+
+            <div className={`${styles.dropdown} ${mostrarDropdown ? '' : styles.hide}`}>
+                <button className={`${styles.button} ${styles.ddbutton}`}>Inicio</button>
+                <button className={`${styles.button} ${styles.ddbutton}`}>Perfil</button>
+                <button className={`${styles.button} ${styles.ddbutton}`}>Apps</button>
+                <button className={`${styles.button} ${styles.ddbutton} `}>Admin</button>
+            </div>
+        </div>
     )
 }
 
